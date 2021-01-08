d98f53fe9d237f50a523f58a17a0b185f1b6859d
YusukeIwaki <iwaki@i3-systems.com>
2021-01-08 16:04:07 +0900

    Implement BrowserType#launch with block, like puppeteer-ruby

--- lib/playwright/channel_owners/browser_type.rb
+++ lib/playwright/channel_owners/browser_type.rb
@@ -4,8 +4,19 @@ module Playwright
       name: 'name',
       executable_path: 'executablePath'
 
-    def launch(options)
-      @channel.send_message_to_server('launch', options.compact)
+    def launch(options, &block)
+      browser = @channel.send_message_to_server('launch', options.compact)
+
+      if block
+        browser_api = ::Playwright::PlaywrightApi.from_channel_owner(browser)
+        begin
+          block.call(browser_api)
+        ensure
+          browser.close
+        end
+      else
+        browser
+      end
     end
   end
 end
