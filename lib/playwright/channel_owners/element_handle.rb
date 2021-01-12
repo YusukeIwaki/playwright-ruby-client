a4a511a0c35b50b5ffe00dc3a4c856b622eedb7b
YusukeIwaki <iwaki@i3-systems.com>
2021-01-12 23:41:31 +0900

    add Empty JSHandle, ElementHandle

---
+++ lib/playwright/channel_owners/element_handle.rb
@@ -0,0 +1,8 @@
+require_relative './js_handle'
+
+module Playwright
+  module ChannelOwners
+    class ElementHandle < JSHandle
+    end
+  end
+end
---
+++ lib/playwright/channel_owners/js_handle.rb
@@ -0,0 +1,4 @@
+module Playwright
+  define_channel_owner :JSHandle do
+  end
+end
