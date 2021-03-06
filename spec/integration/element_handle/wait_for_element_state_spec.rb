67755e02538e911bf91a2b07ca8ca57d6264fb79
YusukeIwaki <iwaki@i3-systems.com>
2021-03-06 20:10:58 +0900

    avoid `undefined:1` error with sleep

--- spec/integration/element_handle/wait_for_element_state_spec.rb
+++ spec/integration/element_handle/wait_for_element_state_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe 'ElementHandle#wait_for_element_state' do
   def give_it_a_chance_to_resolve(page)
     5.times do
+      sleep 0.04 # wait a bit for avoiding `undefined:1` error.
       page.evaluate('() => new Promise(f => requestAnimationFrame(() => requestAnimationFrame(f)))')
     end
   end
