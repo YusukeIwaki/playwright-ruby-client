d239ca39e2d551aef96b772f11c81474ecbbfdae
YusukeIwaki <iwaki@i3-systems.com>
2021-04-14 11:54:48 +0900

    set timeout during tracing

--- spec/integration/tracing_spec.rb
+++ spec/integration/tracing_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'securerandom'
+require 'timeout'
 require 'tmpdir'
 
 # https://github.com/microsoft/playwright/blob/master/tests/chromium/tracing.spec.ts
@@ -12,11 +13,19 @@ RSpec.describe 'tracing' do
     File.delete(output_trace_file) if File.exist?(output_trace_file)
   end
 
+  def tracing_in(browser, page, **kwargs, &block)
+    Timeout.timeout(7) do
+      browser.start_tracing(page: page, **kwargs)
+      block.call
+      browser.stop_tracing
+    end
+  end
+
   it 'should output a trace', sinatra: true do
     with_page do |page|
-      browser.start_tracing(page: page, screenshots: true, path: output_trace_file)
-      page.goto("#{server_prefix}/grid.html")
-      browser.stop_tracing
+      tracing_in(browser, page, screenshots: true, path: output_trace_file) do
+        page.goto("#{server_prefix}/grid.html")
+      end
       expect(File.exist?(output_trace_file)).to eq(true)
     end
   end
@@ -25,9 +34,9 @@ RSpec.describe 'tracing' do
     with_page do |page|
       Dir.mktmpdir do |dir|
         file_path = File.join(dir, 'these', 'are', 'directories', output_trace_file)
-        browser.start_tracing(page: page, screenshots: true, path: file_path)
-        page.goto("#{server_prefix}/grid.html")
-        browser.stop_tracing
+        tracing_in(browser, page, screenshots: true, path: file_path) do
+          page.goto("#{server_prefix}/grid.html")
+        end
         expect(File.exist?(file_path)).to eq(true)
       end
     end
@@ -35,13 +44,9 @@ RSpec.describe 'tracing' do
 
   it 'should run with custom categories if provided', sinatra: true do
     with_page do |page|
-      browser.start_tracing(
-        page: page,
-        path: output_trace_file,
-        categories: ['disabled-by-default-v8.cpu_profiler.hires'],
-      )
-      page.goto("#{server_prefix}/grid.html")
-      browser.stop_tracing
+      tracing_in(browser, page, path: output_trace_file, categories: ['disabled-by-default-v8.cpu_profiler.hires']) do
+        page.goto("#{server_prefix}/grid.html")
+      end
 
       trace_json = JSON.parse(File.read(output_trace_file))
       expect(trace_json.dig('metadata', 'trace-config')).to include('disabled-by-default-v8.cpu_profiler.hires')
@@ -61,27 +66,27 @@ RSpec.describe 'tracing' do
 
   it 'should return a buffer', sinatra: true do
     with_page do |page|
-      browser.start_tracing(page: page, screenshots: true, path: output_trace_file)
-      page.goto("#{server_prefix}/grid.html")
-      trace = browser.stop_tracing
+      trace = tracing_in(browser, page, screenshots: true, path: output_trace_file) do
+        page.goto("#{server_prefix}/grid.html")
+      end
       expect(trace).to eq(File.read(output_trace_file))
     end
   end
 
   it 'should work without options', sinatra: true do
     with_page do |page|
-      browser.start_tracing(page: page)
-      page.goto("#{server_prefix}/grid.html")
-      trace = browser.stop_tracing
+      trace = tracing_in(browser, page) do
+        page.goto("#{server_prefix}/grid.html")
+      end
       expect(trace).to be_a(String)
     end
   end
 
   it 'should support a buffer without a path', sinatra: true do
     with_page do |page|
-      browser.start_tracing(page: page, screenshots: true)
-      page.goto("#{server_prefix}/grid.html")
-      trace = browser.stop_tracing
+      trace = tracing_in(browser, page, screenshots: true) do
+        page.goto("#{server_prefix}/grid.html")
+      end
       expect(trace).to include('screenshot')
     end
   end
