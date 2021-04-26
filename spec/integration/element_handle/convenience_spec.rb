6802617a65d42f89fa822294eef1150e19dcc089
YusukeIwaki <iwaki@i3-systems.com>
2021-04-26 11:22:59 +0900

    add more specs

--- spec/integration/element_handle/convenience_spec.rb
+++ spec/integration/element_handle/convenience_spec.rb
@@ -16,214 +16,266 @@ RSpec.describe 'ElementHandle' do
     end
   end
 
-  # it('getAttribute should work', async ({ page, server }) => {
-  #   await page.goto(`${server.PREFIX}/dom.html`);
-  #   const handle = await page.$('#outer');
-  #   expect(await handle.getAttribute('name')).toBe('value');
-  #   expect(await handle.getAttribute('foo')).toBe(null);
-  #   expect(await page.getAttribute('#outer', 'name')).toBe('value');
-  #   expect(await page.getAttribute('#outer', 'foo')).toBe(null);
-  # });
+  it 'get_attribute should work', sinatra: true do
+    with_page do |page|
+      page.goto("#{server_prefix}/dom.html")
+      handle = page.query_selector('#outer')
+      expect(handle['name']).to eq('value')
+      expect(handle['foo']).to be_nil
+      expect(page.get_attribute('#outer', 'name')).to eq('value')
+      expect(page.get_attribute('#outer', 'foo')).to be_nil
+    end
+  end
 
-  # it('innerHTML should work', async ({ page, server }) => {
-  #   await page.goto(`${server.PREFIX}/dom.html`);
-  #   const handle = await page.$('#outer');
-  #   expect(await handle.innerHTML()).toBe('<div id="inner">Text,\nmore text</div>');
-  #   expect(await page.innerHTML('#outer')).toBe('<div id="inner">Text,\nmore text</div>');
-  # });
+  it 'inner_html should work', sinatra: true do
+    with_page do |page|
+      page.goto("#{server_prefix}/dom.html")
+      handle = page.query_selector('#outer')
+      expect(handle.inner_html).to eq("<div id=\"inner\">Text,\nmore text</div>")
+      expect(page.inner_html('#outer')).to eq("<div id=\"inner\">Text,\nmore text</div>")
+    end
+  end
 
-  # it('innerText should work', async ({ page, server }) => {
-  #   await page.goto(`${server.PREFIX}/dom.html`);
-  #   const handle = await page.$('#inner');
-  #   expect(await handle.innerText()).toBe('Text, more text');
-  #   expect(await page.innerText('#inner')).toBe('Text, more text');
-  # });
+  it 'inner_text should work', sinatra: true do
+    with_page do |page|
+      page.goto("#{server_prefix}/dom.html")
+      handle = page.query_selector('#inner')
+      expect(handle.inner_text).to eq("Text, more text")
+      expect(page.inner_text('#inner')).to eq("Text, more text")
+    end
+  end
 
-  # it('innerText should throw', async ({ page, server }) => {
-  #   await page.setContent(`<svg>text</svg>`);
-  #   const error1 = await page.innerText('svg').catch(e => e);
-  #   expect(error1.message).toContain('Not an HTMLElement');
-  #   const handle = await page.$('svg');
-  #   const error2 = await handle.innerText().catch(e => e);
-  #   expect(error2.message).toContain('Not an HTMLElement');
-  # });
+  it 'inner_text should throw' do
+    with_page do |page|
+      page.content = '<svg>text</svg>'
+      handle = page.query_selector('svg')
+      expect { handle.inner_text }.to raise_error(/Not an HTMLElement/)
+      expect { page.inner_text('svg') }.to raise_error(/Not an HTMLElement/)
+    end
+  end
 
-  # it('textContent should work', async ({ page, server }) => {
-  #   await page.goto(`${server.PREFIX}/dom.html`);
-  #   const handle = await page.$('#inner');
-  #   expect(await handle.textContent()).toBe('Text,\nmore text');
-  #   expect(await page.textContent('#inner')).toBe('Text,\nmore text');
-  # });
+  it 'text_content should work', sinatra: true do
+    with_page do |page|
+      page.goto("#{server_prefix}/dom.html")
+      handle = page.query_selector('#inner')
+      expect(handle.text_content).to eq("Text,\nmore text")
+      expect(page.text_content('#inner')).to eq("Text,\nmore text")
+    end
+  end
 
-  # it('textContent should be atomic', async ({ playwright, page }) => {
-  #   const createDummySelector = () => ({
-  #     query(root, selector) {
-  #       const result = root.querySelector(selector);
-  #       if (result)
-  #         Promise.resolve().then(() => result.textContent = 'modified');
-  #       return result;
-  #     },
-  #     queryAll(root: HTMLElement, selector: string) {
-  #       const result = Array.from(root.querySelectorAll(selector));
-  #       for (const e of result)
-  #         Promise.resolve().then(() => e.textContent = 'modified');
-  #       return result;
-  #     }
-  #   });
-  #   await playwright.selectors.register('textContent', createDummySelector);
-  #   await page.setContent(`<div>Hello</div>`);
-  #   const tc = await page.textContent('textContent=div');
-  #   expect(tc).toBe('Hello');
-  #   expect(await page.evaluate(() => document.querySelector('div').textContent)).toBe('modified');
-  # });
+  it 'text_content should be atomic' do
+    script = <<~JAVASCRIPT
+    {
+      query(root, selector) {
+        const result = root.querySelector(selector);
+        if (result)
+          Promise.resolve().then(() => result.textContent = 'modified');
+        return result;
+      },
+      queryAll(root, selector) {
+        const result = Array.from(root.querySelectorAll(selector));
+        for (const e of result)
+          Promise.resolve().then(() => e.textContent = 'modified');
+        return result;
+      }
+    }
+    JAVASCRIPT
 
-  # it('innerText should be atomic', async ({ playwright, page }) => {
-  #   const createDummySelector = () => ({
-  #     query(root: HTMLElement, selector: string) {
-  #       const result = root.querySelector(selector);
-  #       if (result)
-  #         Promise.resolve().then(() => result.textContent = 'modified');
-  #       return result;
-  #     },
-  #     queryAll(root: HTMLElement, selector: string) {
-  #       const result = Array.from(root.querySelectorAll(selector));
-  #       for (const e of result)
-  #         Promise.resolve().then(() => e.textContent = 'modified');
-  #       return result;
-  #     }
-  #   });
-  #   await playwright.selectors.register('innerText', createDummySelector);
-  #   await page.setContent(`<div>Hello</div>`);
-  #   const tc = await page.innerText('innerText=div');
-  #   expect(tc).toBe('Hello');
-  #   expect(await page.evaluate(() => document.querySelector('div').innerText)).toBe('modified');
-  # });
+    playwright.selectors.register('textContent', script: script)
+    with_page do |page|
+      page.content = '<div>Hello</div>'
+      tc = page.text_content('textContent=div')
+      expect(tc).to eq('Hello')
+      expect(page.evaluate("() => document.querySelector('div').textContent")).to eq('modified')
+    end
+  end
 
-  # it('innerHTML should be atomic', async ({ playwright, page }) => {
-  #   const createDummySelector = () => ({
-  #     query(root, selector) {
-  #       const result = root.querySelector(selector);
-  #       if (result)
-  #         Promise.resolve().then(() => result.textContent = 'modified');
-  #       return result;
-  #     },
-  #     queryAll(root: HTMLElement, selector: string) {
-  #       const result = Array.from(root.querySelectorAll(selector));
-  #       for (const e of result)
-  #         Promise.resolve().then(() => e.textContent = 'modified');
-  #       return result;
-  #     }
-  #   });
-  #   await playwright.selectors.register('innerHTML', createDummySelector);
-  #   await page.setContent(`<div>Hello<span>world</span></div>`);
-  #   const tc = await page.innerHTML('innerHTML=div');
-  #   expect(tc).toBe('Hello<span>world</span>');
-  #   expect(await page.evaluate(() => document.querySelector('div').innerHTML)).toBe('modified');
-  # });
 
-  # it('getAttribute should be atomic', async ({ playwright, page }) => {
-  #   const createDummySelector = () => ({
-  #     query(root: HTMLElement, selector: string) {
-  #       const result = root.querySelector(selector);
-  #       if (result)
-  #         Promise.resolve().then(() => result.setAttribute('foo', 'modified'));
-  #       return result;
-  #     },
-  #     queryAll(root: HTMLElement, selector: string) {
-  #       const result = Array.from(root.querySelectorAll(selector));
-  #       for (const e of result)
-  #         Promise.resolve().then(() => (e as HTMLElement).setAttribute('foo', 'modified'));
-  #       return result;
-  #     }
-  #   });
-  #   await playwright.selectors.register('getAttribute', createDummySelector);
-  #   await page.setContent(`<div foo=hello></div>`);
-  #   const tc = await page.getAttribute('getAttribute=div', 'foo');
-  #   expect(tc).toBe('hello');
-  #   expect(await page.evaluate(() => document.querySelector('div').getAttribute('foo'))).toBe('modified');
-  # });
+  it 'inner_text should be atomic' do
+    script = <<~JAVASCRIPT
+    {
+      query(root, selector) {
+        const result = root.querySelector(selector);
+        if (result)
+          Promise.resolve().then(() => result.textContent = 'modified');
+        return result;
+      },
+      queryAll(root, selector) {
+        const result = Array.from(root.querySelectorAll(selector));
+        for (const e of result)
+          Promise.resolve().then(() => e.textContent = 'modified');
+        return result;
+      }
+    }
+    JAVASCRIPT
 
-  # it('isVisible and isHidden should work', async ({ page }) => {
-  #   await page.setContent(`<div>Hi</div><span></span>`);
+    playwright.selectors.register('innerText', script: script)
+    with_page do |page|
+      page.content = '<div>Hello</div>'
+      tc = page.inner_text('innerText=div')
+      expect(tc).to eq('Hello')
+      expect(page.evaluate("() => document.querySelector('div').innerText")).to eq('modified')
+    end
+  end
 
-  #   const div = await page.$('div');
-  #   expect(await div.isVisible()).toBe(true);
-  #   expect(await div.isHidden()).toBe(false);
-  #   expect(await page.isVisible('div')).toBe(true);
-  #   expect(await page.isHidden('div')).toBe(false);
+  it 'inner_html should be atomic' do
+    script = <<~JAVASCRIPT
+    {
+      query(root, selector) {
+        const result = root.querySelector(selector);
+        if (result)
+          Promise.resolve().then(() => result.textContent = 'modified');
+        return result;
+      },
+      queryAll(root, selector) {
+        const result = Array.from(root.querySelectorAll(selector));
+        for (const e of result)
+          Promise.resolve().then(() => e.textContent = 'modified');
+        return result;
+      }
+    }
+    JAVASCRIPT
 
-  #   const span = await page.$('span');
-  #   expect(await span.isVisible()).toBe(false);
-  #   expect(await span.isHidden()).toBe(true);
-  #   expect(await page.isVisible('span')).toBe(false);
-  #   expect(await page.isHidden('span')).toBe(true);
+    playwright.selectors.register('innerHTML', script: script)
+    with_page do |page|
+      page.content = '<div>Hello<span>world</span></div>'
+      tc = page.inner_html('innerHTML=div')
+      expect(tc).to eq('Hello<span>world</span>')
+      expect(page.evaluate("() => document.querySelector('div').innerHTML")).to eq('modified')
+    end
+  end
 
-  #   expect(await page.isVisible('no-such-element')).toBe(false);
-  #   expect(await page.isHidden('no-such-element')).toBe(true);
-  # });
+  it 'get_attribute should be atomic' do
+    script = <<~JAVASCRIPT
+    {
+      query(root, selector) {
+        const result = root.querySelector(selector);
+        if (result)
+          Promise.resolve().then(() => result.setAttribute('foo', 'modified'));
+        return result;
+      },
+      queryAll(root, selector) {
+        const result = Array.from(root.querySelectorAll(selector));
+        for (const e of result)
+          Promise.resolve().then(() => e.setAttribute('foo', 'modified'));
+        return result;
+      }
+    }
+    JAVASCRIPT
 
-  # it('element state checks should work for label with zero-sized input', async ({page, server}) => {
-  #   await page.setContent(`
-  #     <label>
-  #       Click me
-  #       <input disabled style="width:0;height:0;padding:0;margin:0;border:0;">
-  #     </label>
-  #   `);
-  #   // Visible checks the label.
-  #   expect(await page.isVisible('text=Click me')).toBe(true);
-  #   expect(await page.isHidden('text=Click me')).toBe(false);
+    playwright.selectors.register('getAttribute', script: script)
+    with_page do |page|
+      page.content = '<div foo=hello></div>'
+      tc = page.get_attribute('getAttribute=div', 'foo')
+      expect(tc).to eq('hello')
+      expect(page.evaluate("() => document.querySelector('div').getAttribute('foo')")).to eq('modified')
+    end
+  end
 
-  #   // Enabled checks the input.
-  #   expect(await page.isEnabled('text=Click me')).toBe(false);
-  #   expect(await page.isDisabled('text=Click me')).toBe(true);
-  # });
+  it 'visible? and hidden? should work' do
+    with_page do |page|
+      page.content = '<div>Hi</div><span></span>'
 
-  # it('isEnabled and isDisabled should work', async ({ page }) => {
-  #   await page.setContent(`
-  #     <button disabled>button1</button>
-  #     <button>button2</button>
-  #     <div>div</div>
-  #   `);
-  #   const div = await page.$('div');
-  #   expect(await div.isEnabled()).toBe(true);
-  #   expect(await div.isDisabled()).toBe(false);
-  #   expect(await page.isEnabled('div')).toBe(true);
-  #   expect(await page.isDisabled('div')).toBe(false);
-  #   const button1 = await page.$(':text("button1")');
-  #   expect(await button1.isEnabled()).toBe(false);
-  #   expect(await button1.isDisabled()).toBe(true);
-  #   expect(await page.isEnabled(':text("button1")')).toBe(false);
-  #   expect(await page.isDisabled(':text("button1")')).toBe(true);
-  #   const button2 = await page.$(':text("button2")');
-  #   expect(await button2.isEnabled()).toBe(true);
-  #   expect(await button2.isDisabled()).toBe(false);
-  #   expect(await page.isEnabled(':text("button2")')).toBe(true);
-  #   expect(await page.isDisabled(':text("button2")')).toBe(false);
-  # });
+      div = page.query_selector('div')
+      expect(div.visible?).to eq(true)
+      expect(div.hidden?).to eq(false)
+      expect(page.visible?('div')).to eq(true)
+      expect(page.hidden?('div')).to eq(false)
 
-  # it('isEditable should work', async ({ page }) => {
-  #   await page.setContent(`<input id=input1 disabled><textarea></textarea><input id=input2>`);
-  #   await page.$eval('textarea', t => t.readOnly = true);
-  #   const input1 = await page.$('#input1');
-  #   expect(await input1.isEditable()).toBe(false);
-  #   expect(await page.isEditable('#input1')).toBe(false);
-  #   const input2 = await page.$('#input2');
-  #   expect(await input2.isEditable()).toBe(true);
-  #   expect(await page.isEditable('#input2')).toBe(true);
-  #   const textarea = await page.$('textarea');
-  #   expect(await textarea.isEditable()).toBe(false);
-  #   expect(await page.isEditable('textarea')).toBe(false);
-  # });
+      span = page.query_selector('span')
+      expect(span.visible?).to eq(false)
+      expect(span.hidden?).to eq(true)
+      expect(page.visible?('span')).to eq(false)
+      expect(page.hidden?('span')).to eq(true)
 
-  # it('isChecked should work', async ({page}) => {
-  #   await page.setContent(`<input type='checkbox' checked><div>Not a checkbox</div>`);
-  #   const handle = await page.$('input');
-  #   expect(await handle.isChecked()).toBe(true);
-  #   expect(await page.isChecked('input')).toBe(true);
-  #   await handle.evaluate(input => input.checked = false);
-  #   expect(await handle.isChecked()).toBe(false);
-  #   expect(await page.isChecked('input')).toBe(false);
-  #   const error = await page.isChecked('div').catch(e => e);
-  #   expect(error.message).toContain('Not a checkbox or radio button');
-  # });
+      expect(page.visible?('no-such-element')).to eq(false)
+      expect(page.hidden?('no-such-element')).to eq(true)
+    end
+  end
+
+  it 'element state checks should work for label with zero-sized input' do
+    with_page do |page|
+      page.content = <<~HTML
+      <label>
+        Click me
+        <input disabled style="width:0;height:0;padding:0;margin:0;border:0;">
+      </label>
+      HTML
+
+      # Visible checks the label.
+      expect(page.visible?('text=Click me')).to eq(true)
+      expect(page.hidden?('text=Click me')).to eq(false)
+
+      # Enabled checks the input.
+      expect(page.enabled?('text=Click me')).to eq(false)
+      expect(page.disabled?('text=Click me')).to eq(true)
+    end
+  end
+
+  it 'enabled? and disabled? should work' do
+    with_page do |page|
+      page.content = <<~HTML
+      <button disabled>button1</button>
+      <button>button2</button>
+      <div>div</div>
+      HTML
+
+      query = 'div'
+      div = page.query_selector(query)
+      expect(div.enabled?).to eq(true)
+      expect(div.disabled?).to eq(false)
+      expect(page.enabled?(query)).to eq(true)
+      expect(page.disabled?(query)).to eq(false)
+
+      query = ':text("button1")'
+      button1 = page.query_selector(query)
+      expect(button1.enabled?).to eq(false)
+      expect(button1.disabled?).to eq(true)
+      expect(page.enabled?(query)).to eq(false)
+      expect(page.disabled?(query)).to eq(true)
+
+      query = ':text("button2")'
+      button2 = page.query_selector(query)
+      expect(button2.enabled?).to eq(true)
+      expect(button2.disabled?).to eq(false)
+      expect(page.enabled?(query)).to eq(true)
+      expect(page.disabled?(query)).to eq(false)
+    end
+  end
+
+  it 'editable? should work' do
+    with_page do |page|
+      page.content = '<input id=input1 disabled><textarea></textarea><input id=input2>'
+      page.eval_on_selector('textarea', 't => t.readOnly = true')
+
+      input1 = page.query_selector('#input1')
+      expect(input1.editable?).to eq(false)
+      expect(page.editable?('#input1')).to eq(false)
+
+      input2 = page.query_selector('#input2')
+      expect(input2.editable?).to eq(true)
+      expect(page.editable?('#input2')).to eq(true)
+
+      textarea = page.query_selector('textarea')
+      expect(textarea.editable?).to eq(false)
+      expect(page.editable?('textarea')).to eq(false)
+    end
+  end
+
+  it 'checked? should work' do
+    with_page do |page|
+      page.content = "<input type='checkbox' checked><div>Not a checkbox</div>"
+      handle = page.query_selector('input')
+
+      expect(handle.checked?).to eq(true)
+      expect(page.checked?('input')).to eq(true)
+
+      handle.evaluate('input => input.checked = false')
+
+      expect(handle.checked?).to eq(false)
+      expect(page.checked?('input')).to eq(false)
+
+      expect { page.checked?('div') }.to raise_error(/Not a checkbox or radio button/)
+    end
+  end
 end
