d6ef3ad5fd0737411c9f67701b8539b4fa0baacc
YusukeIwaki <iwaki@i3-systems.com>
2021-09-28 10:09:04 +0900

    Update error message

--- lib/playwright/version.rb
+++ lib/playwright/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Playwright
-  VERSION = '1.15.0'
-  COMPATIBLE_PLAYWRIGHT_VERSION = '1.15.0'
+  VERSION = '1.16.beta1'
+  COMPATIBLE_PLAYWRIGHT_VERSION = '1.16.0'
 end
