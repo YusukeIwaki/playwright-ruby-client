c88cb8b5279c15187e65d7a005b5e439a2675216
YusukeIwaki <iwaki@i3-systems.com>
2022-03-20 03:07:31 +0900

    Update version and compatible driver version

--- lib/playwright/version.rb
+++ lib/playwright/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Playwright
-  VERSION = '1.19.1'
-  COMPATIBLE_PLAYWRIGHT_VERSION = '1.19.2'
+  VERSION = '1.20.0'
+  COMPATIBLE_PLAYWRIGHT_VERSION = '1.20.0'
 end
