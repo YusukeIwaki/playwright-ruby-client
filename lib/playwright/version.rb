522e0a1d175b8183a73ee165c1e6433de8a2f380
YusukeIwaki <iwaki@i3-systems.com>
2022-05-19 01:27:09 +0900

    Mark 1.22.0

--- lib/playwright/version.rb
+++ lib/playwright/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Playwright
-  VERSION = '1.22.beta1'
-  COMPATIBLE_PLAYWRIGHT_VERSION = '1.22.0'
+  VERSION = '1.22.0'
+  COMPATIBLE_PLAYWRIGHT_VERSION = '1.22.1'
 end
