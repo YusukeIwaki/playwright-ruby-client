8740db9d56edadb1c6f0844131bbd55d85fad1bb
YusukeIwaki <iwaki@i3-systems.com>
2022-08-13 23:03:45 +0900

    Update playwright driver to 1.25.0

--- lib/playwright/version.rb
+++ lib/playwright/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Playwright
-  VERSION = '1.24.0'
-  COMPATIBLE_PLAYWRIGHT_VERSION = '1.24.2'
+  VERSION = '1.25.0'
+  COMPATIBLE_PLAYWRIGHT_VERSION = '1.25.0'
 end
