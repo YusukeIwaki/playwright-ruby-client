01d274834335fc017e009e885421e8a419ae5793
YusukeIwaki <iwaki@i3-systems.com>
2023-09-19 17:52:37 +0900

    fix error message

--- spec/integration/page/request_intercept_spec.rb
+++ spec/integration/page/request_intercept_spec.rb
@@ -201,7 +201,7 @@ RSpec.describe 'request interception', sinatra: true do
         end
       })
       response = page.goto("#{server_prefix}/slow", timeout: 20000)
-      expect(response.body).to eq('Error: Request timed out after 10ms')
+      expect(response.body).to eq('Request timed out after 10ms')
     end
   end
 
