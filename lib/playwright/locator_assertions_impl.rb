97fdeaab1a3ab61acd88f47c4efa9da06978eb62
YusukeIwaki <iwaki@i3-systems.com>
2023-11-03 08:37:40 +0900

    Make locator assertion Ruby <= 2.7 compatible

--- lib/playwright/locator_assertions_impl.rb
+++ lib/playwright/locator_assertions_impl.rb
@@ -1,10 +1,14 @@
 module Playwright
   # ref: https://github.com/microsoft/playwright-python/blob/main/playwright/_impl/_assertions.py
   define_api_implementation :LocatorAssertionsImpl do
-    def self.define_negation(method_name)
-      define_method(:"not_#{method_name}") do |*args, **kwargs|
-        self.not.send(method_name, *args, **kwargs)
-      end 
+    def self._define_negation(method_name)
+      define_method("not_#{method_name}") do |*args, **kwargs|
+        if kwargs.empty? # for Ruby < 2.7
+          _not.public_send(method_name, *args)
+        else
+          _not.public_send(method_name, *args, **kwargs)
+        end
+      end
     end
 
     def initialize(locator, timeout, is_not, message)
@@ -52,8 +56,8 @@ module Playwright
       end
     end
 
-    private def not()
-      self.class.new(
+    private def _not # "not" is reserved in Ruby
+      LocatorAssertionsImpl.new(
         @locator,
         @timeout,
         !@is_not,
@@ -114,8 +118,8 @@ module Playwright
           "to.contain.text.array",
           {
             expectedText: expected_text,
-            useInnerText:,
-            timeout:,
+            useInnerText: useInnerText,
+            timeout: timeout,
           },
           expected,
           "Locator expected to contain text"
@@ -125,22 +129,22 @@ module Playwright
           [expected],
           true,
           true,
-          ignoreCase 
+          ignoreCase
         )
 
         expect_impl(
           "to.have.text",
           {
             expectedText: expected_text,
-            useInnerText:,
-            timeout:,
+            useInnerText: useInnerText,
+            timeout: timeout,
           },
           expected,
           "Locator expected to contain text"
         )
       end
     end
-    define_negation :to_contain_text
+    _define_negation :to_contain_text
 
     def to_have_attribute(name, value, timeout: nil)
       expected_text = to_expected_text_values([value])
@@ -149,13 +153,13 @@ module Playwright
         {
           expressionArg: name,
           expectedText: expected_text,
-          timeout:,
+          timeout: timeout,
         },
         value,
         "Locator expected to have attribute"
       )
     end
-    define_negation :to_have_attribute
+    _define_negation :to_have_attribute
 
     def to_have_class(expected, timeout: nil)
       if expected.respond_to?(:each)
@@ -164,7 +168,7 @@ module Playwright
           "to.have.class.array",
           {
             expectedText: expected_text,
-            timeout:,
+            timeout: timeout,
           },
           expected,
           "Locator expected to have class"
@@ -175,27 +179,27 @@ module Playwright
           "to.have.class",
           {
             expectedText: expected_text,
-            timeout:,
+            timeout: timeout,
           },
           expected,
           "Locator expected to have class"
         )
       end
     end
-    define_negation :to_have_class
+    _define_negation :to_have_class
 
     def to_have_count(count, timeout: nil)
       expect_impl(
         "to.have.count",
         {
           expectedNumber: count,
-          timeout:,
+          timeout: timeout,
         },
         count,
         "Locator expected to have count"
       )
     end
-    define_negation :to_have_count
+    _define_negation :to_have_count
 
     def to_have_css(name, value, timeout: nil)
       expected_text = to_expected_text_values([value])
@@ -204,13 +208,13 @@ module Playwright
         {
           expressionArg: name,
           expectedText: expected_text,
-          timeout:,
+          timeout: timeout,
         },
         value,
         "Locator expected to have CSS"
       )
     end
-    define_negation :to_have_css
+    _define_negation :to_have_css
 
     def to_have_id(id, timeout: nil)
       expected_text = to_expected_text_values([id])
@@ -218,13 +222,13 @@ module Playwright
         "to.have.id",
         {
           expectedText: expected_text,
-          timeout:
+          timeout: timeout,
         },
         id,
         "Locator expected to have ID"
       )
     end
-    define_negation :to_have_id
+    _define_negation :to_have_id
 
     def to_have_js_property(name, value, timeout: nil)
       expect_impl(
@@ -232,13 +236,13 @@ module Playwright
         {
           expressionArg: name,
           expectedValue: value,
-          timeout:,
+          timeout: timeout,
         },
         value,
         "Locator expected to have JS Property"
       )
     end
-    define_negation :to_have_js_property
+    _define_negation :to_have_js_property
 
     def to_have_value(value, timeout: nil)
       expected_text = to_expected_text_values([value])
@@ -247,28 +251,28 @@ module Playwright
         "to.have.value",
         {
           expectedText: expected_text,
-          timeout:,
+          timeout: timeout,
         },
         value,
         "Locator expected to have Value"
       )
     end
-    define_negation :to_have_value
+    _define_negation :to_have_value
 
     def to_have_values(values, timeout: nil)
       expected_text = to_expected_text_values(values)
-      
+
       expect_impl(
         "to.have.values",
         {
           expectedText: expected_text,
-          timeout:,
+          timeout: timeout,
         },
         values,
         "Locator expected to have Values"
       )
     end
-    define_negation :to_have_values
+    _define_negation :to_have_values
 
     def to_have_text(expected, ignoreCase: nil, timeout: nil, useInnerText: nil)
       if expected.respond_to?(:each)
@@ -282,8 +286,8 @@ module Playwright
           "to.have.text.array",
           {
             expectedText: expected_text,
-            userInnerText: useInnerText,
-            timeout:,
+            useInnerText: useInnerText,
+            timeout: timeout,
           },
           expected,
           "Locator expected to have text"
@@ -300,114 +304,114 @@ module Playwright
           {
             expectedText: expected_text,
             useInnerText: useInnerText,
-            timeout:,
+            timeout: timeout,
           },
           expected,
           "Locator expected to have text"
         )
       end
     end
-    define_negation :to_have_text
+    _define_negation :to_have_text
 
     def to_be_attached(attached: nil, timeout: nil)
       expect_impl(
         (attached || attached.nil?) ? "to.be.attached" : "to.be.detached",
-        { timeout: },
+        { timeout: timeout },
         nil,
         "Locator expected to be attached"
       )
     end
-    define_negation :to_be_attached
+    _define_negation :to_be_attached
 
     def to_be_checked(checked: nil, timeout: nil)
       expect_impl(
         (checked || checked.nil?) ? "to.be.checked" : "to.be.unchecked",
-        { timeout: },
+        { timeout: timeout },
         nil,
         "Locator expected to be checked"
       )
     end
-    define_negation :to_be_checked
+    _define_negation :to_be_checked
 
     def to_be_disabled(timeout: nil)
       expect_impl(
         "to.be.disabled",
-        { timeout: },
+        { timeout: timeout },
         nil,
         "Locator expected to be disabled"
       )
     end
-    define_negation :to_be_disabled
+    _define_negation :to_be_disabled
 
     def to_be_editable(editable: nil, timeout: nil)
       expect_impl(
         (editable || editable.nil?) ? "to.be.editable" : "to.be.readonly",
-        { timeout: },
+        { timeout: timeout },
         nil,
         "Locator expected to be editable"
       )
     end
-    define_negation :to_be_editable
+    _define_negation :to_be_editable
 
     def to_be_empty(timeout: nil)
       expect_impl(
         "to.be.empty",
-        { timeout: },
+        { timeout: timeout },
         nil,
         "Locator expected to be empty"
       )
     end
-    define_negation :to_be_empty
+    _define_negation :to_be_empty
 
     def to_be_enabled(enabled: nil, timeout: nil)
       expect_impl(
         (enabled || enabled.nil?) ? "to.be.enabled" : "to.be.disabled",
-        { timeout: },
+        { timeout: timeout },
         nil,
         "Locator expected to be enabled"
       )
     end
-    define_negation :to_be_enabled
+    _define_negation :to_be_enabled
 
     def to_be_hidden(timeout: nil)
       expect_impl(
         "to.be.hidden",
-        { timeout: },
+        { timeout: timeout },
         nil,
         "Locator expected to be hidden"
       )
     end
-    define_negation :to_be_hidden
+    _define_negation :to_be_hidden
 
     def to_be_visible(timeout: nil, visible: nil)
       expect_impl(
         (visible || visible.nil?) ? "to.be.visible" : "to.be.hidden",
-        { timeout: },
+        { timeout: timeout },
         nil,
         "Locator expected to be visible"
       )
     end
-    define_negation :to_be_visible
+    _define_negation :to_be_visible
 
     def to_be_focused(timeout: nil)
       expect_impl(
         "to.be.focused",
-        { timeout: },
+        { timeout: timeout },
         nil,
         "Locator expected to be focused"
       )
     end
-    define_negation :to_be_focused
+    _define_negation :to_be_focused
 
     def to_be_in_viewport(ratio: nil, timeout: nil)
       expect_impl(
         "to.be.in.viewport",
-        { timeout:, expectedNumber: ratio },
+        { timeout: timeout, expectedNumber: ratio },
         nil,
         "Locator expected to be in viewport"
       )
     end
-    define_negation :to_be_in_viewport
+    _define_negation :to_be_in_viewport
 
   end
 end
