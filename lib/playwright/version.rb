2f63e27d23eeb7f10e34553c2cb5d8e4836071e5
YusukeIwaki <iwaki@i3-systems.com>
2024-02-10 16:59:23 +0900

    Update Playwright driver to 1.42.0-alpha-2024-02-10

--- lib/playwright/version.rb
+++ lib/playwright/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Playwright
-  VERSION = '1.41.beta1'
-  COMPATIBLE_PLAYWRIGHT_VERSION = '1.41.0-alpha-dec-24-2023'
+  VERSION = '1.42.beta1'
+  COMPATIBLE_PLAYWRIGHT_VERSION = '1.42.0-alpha-2024-02-10'
 end
