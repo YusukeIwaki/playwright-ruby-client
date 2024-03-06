aeb9671a1b13eeff0977c27af1c1d93879ba9bcd
YusukeIwaki <iwaki@i3-systems.com>
2024-03-07 03:01:46 +0900

    Mark 1.42.1

--- lib/playwright/version.rb
+++ lib/playwright/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Playwright
-  VERSION = '1.42.0'
-  COMPATIBLE_PLAYWRIGHT_VERSION = '1.42.0'
+  VERSION = '1.42.1'
+  COMPATIBLE_PLAYWRIGHT_VERSION = '1.42.1'
 end
